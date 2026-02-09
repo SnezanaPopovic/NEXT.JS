b7edf041b0a03619e1f4583498ae375585b595ab
Snezana Popovic <popovicsnezana45@gmail.com>
2026-02-09 19:23:27 +0100

    /

--- app/error.tsx
+++ app/error.tsx
@@ -1,4 +1,15 @@
 "use client";
-export default function error() {
-  return <div>ERROR</div>;
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return (
+    <div>
+      <h2>Nešto je pošlo po zlu</h2>
+      <button onClick={reset}>Pokušaj ponovo</button>
+    </div>
+  );
 }
